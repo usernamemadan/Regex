7dff4d8d550fa6c0f0db2a50f6166a1c8399f4b0
madan <madan@local.com>
2021-09-16 23:44:03 +0530

    added code to validate mobile number

--- src/com/regex/UserRegistration.java
+++ src/com/regex/UserRegistration.java
@@ -23,13 +23,22 @@ public class UserRegistration {
 		}
 
 		String EmailRegex = "^[a-z]{3}[[\\.][a-z]{3}]*\\@[a-z]{2}\\.[a-z]{2}[\\.[a-z]{2}]*";
-		String Email = input("LastName");
+		String Email = input("Email");
 		if (Pattern.matches(EmailRegex, Email)) { 
 			System.out.println("Email is valid");
 		} else {
 			System.out.println("Email is invalid");
 		}
-	
+		
+		String MobileRegex = "[1-9][0-9]\\s[1-9][0-9]{9}";
+		String Mobile = input("Mobile number");
+		if (Pattern.matches(MobileRegex, Mobile)) { 
+			System.out.println("Mobile number is valid");
+		} else {
+			System.out.println("Mobile number is invalid");
+		}
+		
+		
 
 	}
 	public static String input(String field) {
