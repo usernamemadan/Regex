3356eb908c23367a1bddec6cbdc53f4942c6d2c6
madan <madan@local.com>
2021-09-16 23:20:40 +0530

    added code to check for valid last name

--- src/com/regex/UserRegistration.java
+++ src/com/regex/UserRegistration.java
@@ -7,12 +7,19 @@ public class UserRegistration {
 
 	public static void main(String[] args) {
 		String firstNameRegex = "^[A-Z][a-z]{2}[a-z]*";
-		
 		String firstName = input("firstName");
 		if (Pattern.matches(firstNameRegex, firstName)) { 
 			System.out.println("first name is valid");
 		} else {
 			System.out.println("first name is invalid");
+		}
+		
+		String LastNameRegex = "^[A-Z][a-z]{2}[a-z]*";
+		String LastName = input("LastName");
+		if (Pattern.matches(firstNameRegex, firstName)) { 
+			System.out.println("Last name is valid");
+		} else {
+			System.out.println("Last name is invalid");
 		}
 	
 
