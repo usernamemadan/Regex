61bb37d805e51ea9a740b275145e5491132fd508
madan <madan@local.com>
2021-09-16 23:16:16 +0530

    added the code to check for valid first name

---
+++ src/com/regex/UserRegistration.java
@@ -0,0 +1,25 @@
+package com.regex;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		String firstNameRegex = "^[A-Z][a-z]{2}[a-z]*";
+		
+		String firstName = input("firstName");
+		if (Pattern.matches(firstNameRegex, firstName)) { 
+			System.out.println("first name is valid");
+		} else {
+			System.out.println("first name is invalid");
+		}
+	
+
+	}
+	public static String input(String field) {
+		System.out.println("Enter the " + field);
+		Scanner scanner = new Scanner(System.in);
+		return scanner.nextLine();
+	}
+}
